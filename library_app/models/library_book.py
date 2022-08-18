a855328937a228aaf6653b227331ef6f88f3e664
Jaroslav-Pol <jaroslav.polujanskij@gmail.com>
2022-08-18 17:17:22 +0300

    commito pavadinimas
    * commito punktas
    * commito punksda
    * dar vienas
    * dar vienas

---
+++ library_app/models/library_book.py
@@ -0,0 +1,68 @@
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+
+class Book(models.Model):
+    _name = "library.book"
+    _description = "Book"
+    # String fields:
+    name = fields.Char("Title")
+    isbn = fields.Char("ISBN")
+    book_type = fields.Selection(
+        [("paper", "Paperback"),
+         ("hard", "Hardcover"),
+         ("electronic", "Electronic"),
+         ("other", "Other")],
+        "Type")
+    notes = fields.Text("Internal Notes")
+    descr = fields.Html("Description")
+    # Numeric fields:
+    copies = fields.Integer(default=1)
+    avg_rating = fields.Float("Average Rating", (3, 2))
+    price = fields.Monetary("Price", "currency_id")
+    # price helper
+    currency_id = fields.Many2one("res.currency")
+    # Date and time fields:
+    date_published = fields.Date()
+    last_borrow_date = fields.Datetime(
+        "Last Borrowed On",
+        default=lambda self: fields.Datetime.now())
+    # Other fields:
+    active = fields.Boolean("Active?")
+    image = fields.Binary("Cover")
+    # Relational Fields
+    publisher_id = fields.Many2one(
+        "res.partner", string="Publisher")
+    author_ids = fields.Many2many(
+        "res.partner", string="Authors")
+
+    publisher_country_id = fields.Many2one(
+        "res.country", string="Publisher Country",
+        compute="_compute_publisher_country",
+    )
+
+    @api.depends("publisher_id.country_id")
+    def _compute_publisher_country(self):
+        for book in self:
+            book.publisher_country_id = book.publisher_id.country_id
+
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderations = [1, 3] * 6
+            terms = [a * b for a, b in zip(digits[:12],
+                                           ponderations)]
+            remain = sum(terms) % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
+
+    def button_check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise ValidationError(
+                    'Please provide an ISBN13 for %s' % book.name)
+            if book.isbn and not book._check_isbn():
+                raise ValidationError(
+                    '%s is an invalid ISBN' % book.isbn)
+        return True
